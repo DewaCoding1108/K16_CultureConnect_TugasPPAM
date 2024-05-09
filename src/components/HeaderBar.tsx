7611243a2b77fcc2c271d18603c54e16ed4bdc6a
WillyFFS <willyfrans13@gmail.com>
2024-05-09 17:13:47 +0700

    adding cart scree ui

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -4,11 +4,18 @@ import { FONTFAMILY, FONTSIZE, SPACING,COLORS } from "../theme/theme";
 
 interface HeaderBarProps {
   title?: string;
+  description?: string;
 }
-const HeaderBar: React.FC<HeaderBarProps> = ({ title }) => {
+const HeaderBar: React.FC<HeaderBarProps> = ({ title,description}) => {
   return (
-    <View style={styles.HeaderContainer}>
-      <Text style={styles.HeaderText}>{title}</Text>
+    <View>
+      <View style={styles.HeaderContainer}>
+        <Text style={styles.HeaderText}>{title}</Text>
+      </View>
+      <View style={styles.DescriptionContainer}>
+        <Text style={styles.descriptionText}> {description}</Text>
+      </View>
+      <View style={styles.horizontalLine} /> 
     </View>
   );
 };
@@ -17,14 +24,36 @@ export default HeaderBar;
 
 const styles = StyleSheet.create({
   HeaderContainer: {
-    padding: SPACING.space_30,
+    paddingTop: SPACING.space_30,
+    paddingHorizontal: SPACING.space_30,
+    marginTop: SPACING.space_8,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  DescriptionContainer: {
+    paddingHorizontal: SPACING.space_30,
+    marginTop: SPACING.space_8,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
   },
   HeaderText: {
+    marginTop: 20,
     fontFamily: "Poppins-Regular",
-    fontSize: FONTSIZE.size_30,
+    fontSize: FONTSIZE.size_28,
     color:COLORS.primaryBlackHex,
+  },
+  descriptionText: {
+    marginTop: 10,
+    fontFamily: "Poppins-Regular",
+    fontSize: FONTSIZE.size_12,
+    color:COLORS.primaryBlackHex,
+  },
+  horizontalLine: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'black', 
+    marginVertical: SPACING.space_10, 
+    marginHorizontal: SPACING.space_30,
   },
 });
---
+++ src/screen/ChartScreen.tsx
@@ -0,0 +1,52 @@
+import { Button, ScrollView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import HeaderBar from '../components/HeaderBar'
+import PesananCard from '../components/PesananCard'
+import { StatusBar } from "expo-status-bar";
+import ChartButton from '../components/ChartButton';
+
+const DetailScreen = ({navigation}:any) => {
+  return (
+    <View >
+      <HeaderBar title="Pesanan" description="Pesan sekarang untuk pengalaman yang tak terlupakan terhadap karya seni"/>
+      <StatusBar translucent backgroundColor="transparent" />
+      <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.ScrollViewFlex}>
+        <View>
+          <PesananCard buttonPressHandler={()=>{navigation.push('Detail')}} name="Komunitas Batik" location= "Yogyakarta" price={57000} type= "Sanggar Seni" imagelink= {require("../../src/assets/app_images/Home_SanggarSeni.png")}></PesananCard>
+          <PesananCard buttonPressHandler={()=>{navigation.push('Detail')}} name="Komunitas Batik" location= "Yogyakarta" price={57000} type= "Sanggar Seni" imagelink= {require("../../src/assets/app_images/Home_SanggarSeni.png")}></PesananCard>
+          <PesananCard buttonPressHandler={()=>{navigation.push('Detail')}} name="Komunitas Batik" location= "Yogyakarta" price={57000} type= "Sanggar Seni" imagelink= {require("../../src/assets/app_images/Home_SanggarSeni.png")}></PesananCard>
+          <PesananCard buttonPressHandler={()=>{navigation.push('Detail')}} name="Komunitas Batik" location= "Yogyakarta" price={57000} type= "Sanggar Seni" imagelink= {require("../../src/assets/app_images/Home_SanggarSeni.png")}></PesananCard>
+          <PesananCard buttonPressHandler={()=>{navigation.push('Detail')}} name="Komunitas Batik" location= "Yogyakarta" price={57000} type= "Sanggar Seni" imagelink= {require("../../src/assets/app_images/Home_SanggarSeni.png")}></PesananCard>
+          <PesananCard buttonPressHandler={()=>{navigation.push('Detail')}} name="Komunitas Batik" location= "Yogyakarta" price={57000} type= "Sanggar Seni" imagelink= {require("../../src/assets/app_images/Home_SanggarSeni.png")}></PesananCard>
+          <PesananCard buttonPressHandler={()=>{navigation.push('Detail')}} name="Komunitas Batik" location= "Yogyakarta" price={57000} type= "Sanggar Seni" imagelink= {require("../../src/assets/app_images/Home_SanggarSeni.png")}></PesananCard>
+          <PesananCard buttonPressHandler={()=>{navigation.push('Detail')}} name="Komunitas Batik" location= "Yogyakarta" price={57000} type= "Sanggar Seni" imagelink= {require("../../src/assets/app_images/Home_SanggarSeni.png")}></PesananCard>
+          <PesananCard buttonPressHandler={()=>{navigation.push('Detail')}} name="Komunitas Batik" location= "Yogyakarta" price={57000} type= "Sanggar Seni" imagelink= {require("../../src/assets/app_images/Home_SanggarSeni.png")}></PesananCard>
+          <PesananCard buttonPressHandler={()=>{navigation.push('Detail')}} name="Komunitas Batik" location= "Yogyakarta" price={57000} type= "Sanggar Seni" imagelink= {require("../../src/assets/app_images/Home_SanggarSeni.png")}></PesananCard>
+          <PesananCard buttonPressHandler={()=>{navigation.push('Detail')}} name="Komunitas Batik" location= "Yogyakarta" price={57000} type= "Sanggar Seni" imagelink= {require("../../src/assets/app_images/Home_SanggarSeni.png")}></PesananCard>
+        </View>
+        <View style={styles.Bottomheigt}></View>
+      </ScrollView>
+      <View style={styles.ButtonAlignment}>
+        <ChartButton price={57000}></ChartButton>
+      </View>
+    </View>
+  )
+}
+
+export default DetailScreen
+
+const styles = StyleSheet.create({
+  ScrollViewFlex:{
+    flexGrow:1,
+    alignItems: "center",
+  },
+  Bottomheigt:{
+    height:250,
+  },
+  ButtonAlignment: {
+    alignSelf: "center",
+    position:"absolute",
+    bottom: 250,
+
+  }
+})
